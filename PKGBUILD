6719c4406cb8209164bb364f536c326b235cd0a6
ju1ius <jules.bernable@laposte.net>
2011-06-26 20:16:20 +0200

    refactored daemon to use C libinotifytools0 lib

--- PKGBUILD
+++ PKGBUILD
@@ -7,7 +7,7 @@ pkgdesc="A Fluxbox automated XDG Menu"
 arch=('any')
 url="http://github.com/ju1ius/fluxdgmenu"
 license=('GPL')
-depends=('openbox' 'bash' 'inotify-tools' 'python-xdg>=0.19')
+depends=('openbox' 'bash' 'libinotifytools0' 'python-xdg')
 makedepends=('git')
 provides=('fluxdgmenu')
 conflicts=('fluxdgmenu')
@@ -30,13 +30,7 @@ build() {
   msg "Starting make..."
 
   cd "$srcdir/$_gitname"
-
-  install -d -m755 "$pkgdir/usr/lib" || return 1
-  cp -Rp "usr/lib/fluxdgmenu" "$pkgdir/usr/lib" || return 1
-  install -d -m755 "$pkgdir/etc/xdg/menus" || return 1
-  cp -p "etc/xdg/menus/fxm-applications.menu" "$pkgdir/etc/xdg/menus" || return 1
-  install -d -m755 "$pkgdir/usr/share" || return 1
-  cp -Rp "usr/share/desktop-directories" "$pkgdir/usr/share" || return 1
-  install -D -m644 "README.md" "$pkgdir/usr/share/doc/fluxdgmenu/README" || return 1
+  make || return 1
+  sudo make install
 }
 
--- usr/lib/fluxdgmenu/fxm.py
+++ usr/lib/fluxdgmenu/fxm.py
@@ -1,55 +1,145 @@
 #!/usr/bin/python
-# coding=utf-8
+# -*- coding: utf-8 -*-
 
-import os, sys, re
+import os, sys, re, StringIO, sqlite3, ConfigParser
 
-import xdg.Config, xdg.BaseDirectory, xdg.DesktopEntry, xdg.Menu
-from xdg.Exceptions import *
+import xdg.Config, xdg.BaseDirectory, xdg.DesktopEntry, xdg.Menu, xdg.IconTheme
+#from xdg.Exceptions import *
 
+class FluXDGMenu(object):
 
-def print_separator(entry):
-  print '[separator] (-------------------------------)'
+  def __init__(self, output):
+    self.parse_config()
+    self.tree = xdg.Menu.parse('fxm-applications.menu')
+    self.print_all()
+    self.output.close()
 
-def print_submenu(entry):
-  print '[submenu] (%s) <%s>' % (
-    entry.getName().encode('utf-8'),
-    entry.getIcon().encode('utf-8')
-  )
-  print_menu(entry)
-  print '[end]'
+  def parse_config(self):
+    self.config = ConfigParser.RawConfigParser()
+    self.config.readfp(StringIO.StringIO("""
+[Icons]
+show: True
+themes: Minty,gnome-wise,hicolor
+size: 24
+"""))
+    self.config.read([
+      '/etc/fluxdgmenu/menu.conf',
+      os.path.expanduser('~/.fluxbox/fluxdgmenu/menu.conf')
+    ])
 
-def print_exec(entry):
-  cmd = re.sub(' [^ ]*%[fFuUdDnNickvm]', '', entry.DesktopEntry.getExec())
-  if entry.DesktopEntry.getTerminal():
-    cmd = 'x-terminal-emulator -T "%s" -e "%s"' % (
-      entry.DesktopEntry.getName().encode('utf-8'),
-      cmd
+    self.show_icons = self.config.getboolean('Icons', 'show')
+    self.themes = [t.strip() for t in self.config.get('Icons','themes').split(',')]
+    self.themes.reverse()
+    self.themes_key = '-'.join(self.themes)
+    self.icon_size = self.config.getint('Icons', 'size')
+
+    xdg.Config.setWindowManager('fluxbox')
+    for theme in self.themes:
+      xdg.Config.setIconTheme(theme)
+    xdg.Config.setIconSize(self.icon_size)
+
+    self.filter_debian = os.path.isfile('/usr/bin/update-menus')
+
+  def print_all(self):
+    """Prints the menu to output"""
+    self.open_cache()
+    self.print_menu(self.tree)
+    self.close_cache()
+
+  def open_cache(self):
+    if self.show_icons:
+      self.cache_conn = sqlite3.connect(
+        os.path.expanduser('~/.fluxbox/fluxdgmenu/icons.db')
+      )
+      self.cache_conn.execute(
+        "CREATE TABLE IF NOT EXISTS cache(key TEXT, path TEXT)"
+      )
+
+      self.cache_conn.row_factory = sqlite3.Row
+      self.cache_cursor = self.cache_conn.cursor()
+
+  def close_cache(self):
+    if self.show_icons:
+      self.cache_conn.commit()
+      self.cache_cursor.close()
+
+  def print_menu(self, menu):
+    for entry in menu.getEntries():
+      if isinstance(entry, xdg.Menu.Separator):
+        self.print_separator(entry)
+      elif isinstance(entry, xdg.Menu.Menu):
+        self.print_submenu(entry)
+      elif isinstance(entry, xdg.Menu.MenuEntry):
+        self.print_exec(entry)
+
+  def print_separator(self, entry):
+    print '[separator] (-------------------------------)'
+
+  def print_submenu(self, entry):
+    if self.show_icons:
+      print '[submenu] (%s) <%s>' % (
+        entry.getName().encode('utf-8'),
+        self.find_icon(entry.getIcon().encode('utf-8'))
+      )
+    else:
+      print '[submenu] (%s)' % entry.getName().encode('utf-8')
+    self.print_menu(entry)
+    print '[end]'
+
+  def print_exec(self, entry):
+    de = entry.DesktopEntry
+    # Skip Debian specific menu entries
+    if self.filter_debian and de.get('Categories', list=False).startswith('X-Debian'):
+      return
+    # Strip command arguments
+    cmd = re.sub(' [^ ]*%[fFuUdDnNickvm]', '', de.getExec())
+    if de.getTerminal():
+      cmd = 'x-terminal-emulator -T "%s" -e "%s"' % (
+        de.getName().encode('utf-8'),
+        cmd
+      )
+    if self.show_icons:
+      print '  [exec] (%s) {%s} <%s>' % (
+        de.getName().encode('utf-8'),
+        cmd,
+        self.find_icon(de.getIcon().encode('utf-8'))
+      )
+    else:
+      print '  [exec] (%s) {%s}' % (
+        de.getName().encode('utf-8'),
+        cmd
+      )
+
+  def find_icon(self, name):
+    """Finds and cache icons"""
+    key = self.themes_key + ':' + name
+    self.cache_cursor.execute(
+      'SELECT cache.key, cache.path FROM cache WHERE cache.key = ?',
+      [key]
     )
-  print '  [exec] (%s) {%s} <%s>' % (
-    entry.DesktopEntry.getName().encode('utf-8'),
-    cmd,
-    entry.DesktopEntry.getIcon().encode('utf-8')
-  )
-
-def print_menu(menu):
-
-  for entry in menu.getEntries():
-    if isinstance(entry, xdg.Menu.Separator):
-      print_separator(entry)
-    elif isinstance(entry, xdg.Menu.Menu):
-      print_submenu(entry)
-    elif isinstance(entry, xdg.Menu.MenuEntry):
-      print_exec(entry)
+    found = self.cache_cursor.fetchone()
+    if found:
+      return found['path'].encode('utf-8')
+    else:
+      path = xdg.IconTheme.getIconPath(name)
+      if path:
+        self.cache_cursor.execute(
+          'INSERT INTO cache(key, path) VALUES(?,?)',
+          [key, path]
+        )
+        return path.encode('utf-8')
+      else:
+        return ''
 
 
 
-filename = 'fxm-applications.menu'
-if len(sys.argv) > 1:
-  filename = sys.argv[1]
-  if not filename.endswith('.menu'): filename += '.menu'
-
-xdg.Config.setWindowManager('fluxbox')
-menu = xdg.Menu.parse(filename)
-
-print_submenu(menu)
+###########################################################################
+# MAIN
+#--------------------------------------------------------------------------
+if __name__ == "__main__":
+  
+  fluxdgmenu = FluXDGMenu(sys.argv[1])
+#--------------------------------------------------------------------------
+# / MAIN
+###########################################################################
 
